f7415919c061eb2e378fb3e49c7da91f78c6475e
Vladimir Kuzma <kuzma@byteful.ch>
2018-12-16 08:50:24 +0100

    changed import for django1.11

--- pennyblack/options.py
+++ pennyblack/options.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.core.context_processors import csrf
+from django.template.context_processors import csrf
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.http import HttpResponseRedirect
@@ -12,19 +12,19 @@ import exceptions
 class NewsletterReceiverMixin(object):
     """
     Mixin for every object that can receive a newsletter
-    """    
+    """
     def get_email(self):
         if hasattr(self,'email'):
             return self.email
         raise exceptions.NotImplementedError('Need a get_email implementation.')
-    
+
     def on_bounce(self, mail):
         pass
-        
+
 class JobUnitMixin(object):
     """
     Mixin for every object which can be target of a Job
-    """    
+    """
     def create_newsletter(self, form_data=None):
         """
         Creates a newsletter for every NewsletterReceiverMixin
@@ -40,7 +40,7 @@ class JobUnitMixin(object):
         job.save()
         job.create_mails(queryset)
         return job
-        
+
     def get_newsletter_receiver_collections(self):
         """
         Returns a dict of valid receiver collections
@@ -50,13 +50,13 @@ class JobUnitMixin(object):
         get_receiver_filtered_queryset function.
         """
         raise exceptions.NotImplementedError("Override this method in your class!")
-    
+
     def get_receiver_queryset(self):
         raise exceptions.NotImplementedError("Override this method in your class!")
-    
+
     def get_receiver_filtered_queryset(self, **kwargs):
         """
-        Takes the second part of the collections 
+        Takes the second part of the collections
         """
         queryset = self.get_receiver_queryset()
         return queryset
@@ -70,14 +70,14 @@ class JobUnitAdmin(admin.ModelAdmin):
     change_form_template = "admin/pennyblack/jobunit/change_form.html"
     collection_select_form = CollectionSelectForm
     collection_selection_form_extra_fields = dict()
-    
+
     def create_newsletter(self, request, object_id):
         obj = get_object_or_404(self.model, pk=object_id)
         if len(obj.get_newsletter_receiver_collections()) == 1 and len(self.collection_selection_form_extra_fields) == 0:
             # there is only one collection and no options to select
             # -> call create_newsletter directly
             job = obj.create_newsletter()
-            return HttpResponseRedirect(reverse('admin:pennyblack_job_change', args=(job.id,)))            
+            return HttpResponseRedirect(reverse('admin:pennyblack_job_change', args=(job.id,)))
         if request.method == 'POST':
             form = self.collection_select_form(data=request.POST,
                 group_object=obj,
@@ -97,7 +97,7 @@ class JobUnitAdmin(admin.ModelAdmin):
         }
         context.update(csrf(request))
         return render_to_response('admin/pennyblack/jobunit/select_receiver_collection.html',context)
-            
+
     def get_urls(self):
         from django.conf.urls.defaults import patterns, url
         urls = super(JobUnitAdmin, self).get_urls()
